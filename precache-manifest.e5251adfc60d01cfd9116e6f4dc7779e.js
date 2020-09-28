c7eb8178e94583810ebf4dcbc51a1822482c57b5
Jacob Kyle <jkthomps21@gmail.com>
2020-09-27 19:20:23 -0500

    Updates

---
+++ precache-manifest.e5251adfc60d01cfd9116e6f4dc7779e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304c673bbaa3ce13c77d00f69bd531df",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ad0257471196788d49d2",
+    "url": "/React-Portfolio/static/css/main.79cc55cf.chunk.css"
+  },
+  {
+    "revision": "ad5151277bbcc3d3069f",
+    "url": "/React-Portfolio/static/js/2.c4cadae0.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/React-Portfolio/static/js/2.c4cadae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0257471196788d49d2",
+    "url": "/React-Portfolio/static/js/main.62a02b89.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "c2fd92392bdcb8ae13642dbcb682bd54",
+    "url": "/React-Portfolio/static/media/books.c2fd9239.jpg"
+  },
+  {
+    "revision": "1976903dbdfa21be371b5241483f455b",
+    "url": "/React-Portfolio/static/media/burgers.1976903d.jpg"
+  },
+  {
+    "revision": "16de66a8d87c5b216606099fcd2e3378",
+    "url": "/React-Portfolio/static/media/midCenturyBuild.16de66a8.jpg"
+  },
+  {
+    "revision": "386d891fbe17d51a109f9893fd144045",
+    "url": "/React-Portfolio/static/media/news.386d891f.jpg"
+  },
+  {
+    "revision": "93be95498ea8f46b51ab120959ec63f2",
+    "url": "/React-Portfolio/static/media/profMe.93be9549.jpg"
+  },
+  {
+    "revision": "0edab2ebbf4fc3bafc30abdb251c8bbc",
+    "url": "/React-Portfolio/static/media/quiz.0edab2eb.jpg"
+  },
+  {
+    "revision": "c1b83b2572716db2152efea296e2cae0",
+    "url": "/React-Portfolio/static/media/schedules.c1b83b25.jpg"
+  },
+  {
+    "revision": "9b9b0fd7f775d1c00b8ceac3a197ab91",
+    "url": "/React-Portfolio/static/media/weather.9b9b0fd7.jpg"
+  }
+]);
